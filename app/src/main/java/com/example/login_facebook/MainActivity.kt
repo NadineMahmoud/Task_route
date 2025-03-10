f9db5189c7b7a2935461218a597555cae4e04c99
NadineMahmoud <162837441+NadineMahmoud@users.noreply.github.com>
2025-03-11 01:00:16 +0200

    first

---
+++ app/src/main/java/com/example/login_facebook/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.login_facebook
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.login_facebook.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.login.setOnClickListener{
+            val intent = Intent(this, login::class.java)
+            startActivity(intent)
+            finish()
+        }
+      }
+}
